4b2c5f5312ce9f4aa6e989743a9ea8fe0704070a
Anuj7777777 <anuj.pandav7777@gmail.com>
2025-04-05 22:09:43 +0530

    Co-authored-by: WhiteDevil213 <WhiteDevil213@users.noreply.github.com>

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // src/utils/api.js
 
-const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000'}/api`;
+const API_BASE_URL =" https://webblog-f7beb8c9hxdkgac7.centralindia-01.azurewebsites.net/api";
 
 const handleResponse = async (response) => {
   if (!response.ok) {
